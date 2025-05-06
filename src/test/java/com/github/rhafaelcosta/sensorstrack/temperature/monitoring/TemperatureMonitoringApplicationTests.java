ff15af953f9631f5b4e8b5b4874f36ae3e1bcdc4
Rhafael Freitas da Costa <rhafael.costa@lactec.org.br>
2025-05-06 11:40:16 -0300

    Criação do projeto spring boot

---
+++ src/test/java/com/github/rhafaelcosta/sensorstrack/temperature/monitoring/TemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rhafaelcosta.sensorstrack.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
